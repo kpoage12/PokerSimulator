effc9cfe597f7e93a59fbf6376f3c5d54d1fcb19
Kyle Poage <114262791+kpoage12@users.noreply.github.com>
2023-09-19 14:33:02 -0500

    Update Chip.java

--- Chip.java
+++ Chip.java
@@ -1,29 +1,47 @@
+/**
+ * This enum represents all the different chip types
+ */
 public enum Chip {
+    /**
+     * White chips are worth 25 cents
+     */
     WHITE{
         @Override
         public double getAmount(){
             return 0.25;
         }
     },
+    /**
+     * Red chips are worth 50 cents
+     */
     RED{
         @Override
         public double getAmount(){
             return 0.5;
         }
     },
+    /**
+     * Blue chips are worth a dollar
+     */
     BLUE{
         @Override
         public double getAmount(){
             return 1;
         }
     },
+    /**
+     * Black chips are worth two dollars
+     */
     BLACK{
         @Override
         public double getAmount(){
             return 2;
         }
     };
-
+    /**
+     * Returns the value of the chip type
+     * @return the value of the chip type
+     */
     public double getAmount() {
         return 0;
     }
